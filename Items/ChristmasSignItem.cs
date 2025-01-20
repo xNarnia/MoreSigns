60aafb0fdf49b25ceb2be21de56aefd53b4ce4b6
niaeco <7970069+xNarnia@users.noreply.github.com>
2025-01-19 20:02:40 -0500

    Add new signs

---
+++ Items/ChristmasSignItem.cs
@@ -0,0 +1,22 @@
+﻿using MoreSigns.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreSigns.Items
+{
+	public class ChristmasSignItem : BaseSignItem<ChristmasSign>
+	{
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.PineTreeBlock, 6)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+		}
+	}
+}
---
+++ Items/SnowSignItem.cs
@@ -0,0 +1,23 @@
+﻿using MoreSigns.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreSigns.Items
+{
+	public class SnowSignItem : BaseSignItem<SnowSign>
+	{
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.SnowBlock, 6)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+		}
+	}
+}
